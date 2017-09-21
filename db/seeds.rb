3ffaf41f2c20b6b81dda838bd974e7a1f38fe58e
Priit Paluoja <priitpaluoja@gmail.com>
2017-09-21 19:53:59 +0300

    DB seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Base.create(name: 'Luke', host: 'localhost', username: 'Homer', password: 'spiderpig', port: 5050)
+Base.create(name: 'Jake', host: 'localhost', username: 'Frodo', password: 'KaleviPoeg123', port: 5050)
