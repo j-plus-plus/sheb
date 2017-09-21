758bd7be38e96e0dfbf0e1af53dac09f0c5445ed
Priit Paluoja <priitpaluoja@gmail.com>
2017-09-21 20:31:56 +0300

    Simple CRUD

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -2,4 +2,23 @@ class BaseController < ApplicationController
   def index
     @bases = Base.all
   end
+
+  def new
+    @new_base = Base.new
+  end
+
+  def create
+    @new_base = Base.new(base_params)
+    if @new_base.save
+      redirect_to '/bases'
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def base_params
+    params.require(:base).permit(:host, :name, :username, :password, :port)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/bases' => 'base#index'
+  get '/bases/new' => 'base#new'
+  post '/bases' => 'base#create'
 end
